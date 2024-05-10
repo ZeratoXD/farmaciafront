f8219baa567be42cbfa5402cdbac8c8f48945559
ZeratoXD <ivan_cassio_dias@hotmail.com>
2024-05-10 10:56:33 -0300

    Componentes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,26 +1,14 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
-import React, { useContext } from 'react'
-
-
 function Footer() {
+    return (
+        <div className="bg-red-800 text-center border-white border-t-2 p-7">
+          
+            <ol className="flex justify-around ">
+                <p>©️ 2024</p>
+                <p>Termos de Serviço  |  Politica de privacidade | </p>
+            </ol>
 
- 
- (
-      <>
-        <div className="flex justify-center bg-red-700 text-white">
-          <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold'>Farmacia | Copyright:</p>
-            <p className='text-lg'>Acesse nossas redes sociais</p>
-            <div className='flex gap-2'>
-              <LinkedinLogo size={48} weight='bold' />
-              <InstagramLogo size={48} weight='bold' />
-              <FacebookLogo size={48} weight='bold' />
-            </div>
-          </div>
+
         </div>
-      </>
     )
-  }
-
-
+}
 export default Footer
