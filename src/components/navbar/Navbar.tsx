85fbf0fdb0476337d28175e13448e8891cd5dd74
ZeratoXD <ivan_cassio_dias@hotmail.com>
2024-05-10 10:54:36 -0300

    Componentes

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <div className="bg-red-800  p-4 border-b-2 border-white">
+            <ol className="flex flex-row justify-around">
+                <li>Farmacia da Saúde</li>
+                <li>Cadastro</li>
+                <li>Listagem</li>
+            </ol>
+
+
+        </div>
+    )
+}
+export default Navbar
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div className="w-full flex p-5 h-full mt-8">
+            <div>
+                <h1 className="text-6xl mb-8">Farmacia </h1>
+                <p>Olá caros clientes</p>
+            </div>
+            <img  className="w-1/2" src="https://img.freepik.com/fotos-gratis/farmaceutica-jovem-hispanica-sorrindo-confiante-em-pe-com-gesto-de-bracos-cruzados-na-farmacia_839833-7087.jpg?size=626&ext=jpg&ga=GA1.1.1395880969.1709596800&semt=ais"></img>
+        </div>
+    )
+}
+export default Home;
